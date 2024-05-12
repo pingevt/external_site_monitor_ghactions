6e3a1219286d09ae0fff01e25898d68a1ac00f7c
Pete Inge <pete.inge@gmail.com>
2024-05-12 15:03:10 -0400

    pa11y rework to continue testing

--- pa11y.js
+++ pa11y.js
@@ -3,44 +3,45 @@ const fs = require('fs-extra');
 const htmlReporter = require('pa11y/lib/reporters/html');
 const jsonReporter = require('pa11y/lib/reporters/json');
 
-let url = "https://peteinge.com";
-
 console.log(process.env);
 
-// let default_options = {
-//   runners: [
-//     'axe',
-//     'htmlcs'
-//   ],
-//   wait: 500
-// };
+let urls = process.env.urls;
 
 
-// pa11y(url, default_options).then((results) => {
-//   console.log(results);
 
-//   fs.writeJson('./jsonReport.json', results, err => {
-//     if (err) {
-//       console.error(err);
-//     } else {
-//       // file written successfully
-//     }
-//   });
+let default_options = {
+  runners: [
+    'axe',
+    'htmlcs'
+  ],
+  wait: 500
+};
+
+pa11y(urls[0], default_options).then((results) => {
+  console.log(results);
+
+  fs.writeJson('./jsonReport.json', results, err => {
+    if (err) {
+      console.error(err);
+    } else {
+      // file written successfully
+    }
+  });
 
 
-//   const html = htmlReporter.results(results);
-//   // const json = jsonReporter.results(results);
+  const html = htmlReporter.results(results);
+  // const json = jsonReporter.results(results);
 
-//   // console.log([html, json]);
-//   Promise.all([html]).then((values) => {
-//     console.log(values[1]);
+  // console.log([html, json]);
+  Promise.all([html]).then((values) => {
+    // console.log(values[1]);
 
-//     fs.writeFile('./htmlReport.html', values[0], err => {
-//       if (err) {
-//         console.error(err);
-//       } else {
-//         // file written successfully
-//       }
-//     });
-//   });
-// });
+    fs.writeFile('./htmlReport.html', values[0], err => {
+      if (err) {
+        console.error(err);
+      } else {
+        // file written successfully
+      }
+    });
+  });
+});
